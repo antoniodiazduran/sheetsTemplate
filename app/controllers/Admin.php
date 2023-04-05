869734b6de8cd88dda7f6cf95f30fb9ccc0ab2c0
antoniodiazduran <antoniodiazduran@instance-3.us-central1-a.c.diazllc01.internal>
2023-04-05 13:54:59 +0000

    adding pre-selected options for urgency and owners

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -9,6 +9,7 @@ class Admin extends Controller {
     public function ownr() {
                 $this->f3->set('breadcrumbs','owr');
 		$this->f3->set('epoch',$this->f3->get('PARAMS.id'));
+		$this->f3->set('actual', $this->db->exec('SELECT Owner FROM enc_log WHERE Epoch = ?',$this->f3->get('PARAMS.id') ));
 		$owners = $this->db->exec('SELECT names FROM owners ORDER BY names');
 	        $this->f3->set('navs','yes');
 		$this->f3->set('nav_menu','navadmin.htm');
@@ -19,7 +20,8 @@ class Admin extends Controller {
     }
     public function urge() {
                 $this->f3->set('breadcrumbs','urg');
-		$this->f3->set('epoch',$this->f3->get('PARAMS.id'));
+		$this->f3->set('epoch', $this->f3->get('PARAMS.id'));
+		$this->f3->set('actual', $this->db->exec('SELECT Urgency FROM enc_log WHERE Epoch = ?',$this->f3->get('PARAMS.id') ));
 		$urgency = $this->db->exec('SELECT names FROM urgency ORDER BY names');
 	        $this->f3->set('navs','yes');
 		$this->f3->set('nav_menu','navadmin.htm');
@@ -37,13 +39,20 @@ class Admin extends Controller {
 		$name =  $ownr['owner'];
 		$epoch = time();
 		$datet = date('y-m-d h:i',time());
-		// Logging changes
+		// Getting old and new owners emails from the owners table
 		$oldowner = $this->db->exec('SELECT email FROM owners WHERE names = (SELECT owner from enc_log WHERE Epoch = ?)', $id);
 		$newowner = $this->db->exec('SELECT email FROM owners WHERE names = ?', $name);
+		$description = $this->db->exec('SELECT Description FROM enc_log WHERE Epoch = ?',$id);
 		$to =  'casey.kackman@eldorado-ca.com,'.$oldowner[0]['email'].','.$newowner[0]['email'];
+		// Adding notes to the notification email to owner of the record
+		$msg  = ',<p>You are the new owner of <a href="http://35.225.79.133/req/edit/'.$id.'"><b>'.$description[0]['Description'].'</b></a>, Lucky you!!, click on the link to see the details.<br/> ';
+		$msg .= 'This is just for your information, but you can update the record as needed. <br/> ';
+		$msg .= 'Sorry, you cannot delete the task!<p>';
+		$msg .= 'If you wish to add notes to the record, you can click <a href="http://35.225.79.133/nts/edit/'.$id.'">here</a><p>Joey<br/>The admin<p>';
+		// Sending the email
+		$main->sendMail($to,$name.$msg);
+		// Logging changes
 		$this->db->exec('INSERT INTO owner_log (timestamp,datetime,enc_log,names)VALUES(?,?,?,?)',array($epoch,$datet,$id,$name));
-		// Sending a notification email to owner of the record
-		$main->sendMail($to,$name.',<br/>You are the new owner of this task, Lucky you!!, click on the link to see the details.<br/> This is just for your information, but you can update the record as needed. <br/>Sorry, you cannot delete the task! :) <p>http://35.225.79.133/req/edit/'.$id);
 		// Changing the enc_log for engineering support
 		$this->db->exec('UPDATE enc_log SET owner = ? WHERE Epoch = ?', array($name,$id));
 		$this->f3->reroute('/sfadm');
@@ -59,13 +68,18 @@ class Admin extends Controller {
 		$datet = date('y-m-d h:i',time());
 		// Logging changes
 		$this->db->exec('INSERT INTO urgency_log (timestamp,datetime,enc_log,names)VALUES(?,?,?,?)',array($epoch,$datet,$id,$name));
-		// Changing the enc_log for engineering support
+		// Setting the sort criteria
 		$sort = 1;
 		if ($name == 'As available') { $sort = 3; }
 		if ($name == 'See due date') { $sort = 2; }
+		// Changing the enc_log for engineering support
 		$this->db->exec('UPDATE enc_log SET urgency = ?, sort = ? WHERE Epoch = ?', array($name,$sort,$id));
 		// Sending a notification email to owner of the record
-		$main->sendMail('casey.kackman@eldorado-ca.com','Your prioirty has change, click on the link to see the changes.<br/> This is just for your information, but you can update the record as needed.<p>http://35.225.79.133/req/edit/'.$id);
+		$owner = $this->db->exec('SELECT Owner from enc_log WHERE Epoch = ?', $id);
+		$email = $this->db->exec('SELECT email FROM owners WHERE names = (SELECT Owner from enc_log WHERE Epoch = ?)', $id);
+		$msg  = $owner[0]['Owner'].'<p>Your prioirty has change, click on the <a href="http://35.225.79.133/req/edit/'.$id.'">link</a> to see the changes.<br/> ';
+		$msg .= 'This is just for your information, but you can update the record as needed.<p>Joey<br/>The admin<p>';
+		$main->sendMail('casey.kackman@eldorado-ca.com,'.$email[0]['email'],$msg);
 		$this->f3->reroute('/sfadm');
     }
     public function sf() {
