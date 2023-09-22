e9f6078e7360a6d6ff5b33010e8828ee665dd548
antoniodiazduran <antoniodiazduran@instance-3.us-central1-a.c.diazllc01.internal>
2023-09-22 22:29:19 +0000

    adding search capabilities to buyers list

--- app/controllers/MAT/Buyer.php
+++ app/controllers/MAT/Buyer.php
@@ -34,6 +34,7 @@ class Buyer extends \Controller {
                 $this->f3->set('navs','yes');
                 $this->f3->set('nav_menu','navbuyers.htm');
                 $this->f3->set('customer','yes');
+                $this->f3->set('columns','[1,2,3,4,5,6,7,8,9,10,11,12]');
                 $this->f3->set('bgcolor','red');
                 $this->f3->set('headers','materials/headers.htm');
                 $this->f3->set('fields','materials/buyersfields.htm');
